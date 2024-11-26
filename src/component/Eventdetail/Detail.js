b30f7e57ac0b366f641d469fcaab73578afd60af
Tietve <156795036+Tietve@users.noreply.github.com>
2024-11-26 11:21:19 +0700

    Add files via upload

---
+++ src/component/Eventdetail/Detail.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Detail = () => {
+    return (
+        <div style={{
+            width: 257,
+            height: 95,
+            textAlign: 'center',
+            color: 'black',
+            fontSize: 36,
+            fontFamily: 'Inter',
+            fontWeight: '500',
+            lineHeight: 16,
+            wordWrap: 'break-word'
+        }}>Detail</div>
+    <div style={{width: 648, height: 0, border: '1px black solid'}}></div>
+    <div style={{width: 1360, height: 1712}}><span
+        style="color: 'black', fontSize: 40, fontFamily: 'Inter', fontWeight: '500', lineHeight: 60, wordWrap: 'break-word'">Welcome to SBS MTV THE KPOP!<br/></span><span
+        style="color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '500', lineHeight: 60, wordWrap: 'break-word'">Your one-stop destination for the hottest KPOP concerts of the year.<br/><br/>Get ready to experience the electrifying energy of your favorite KPOP stars live on stage! From the latest comebacks to world tours, we bring you exclusive access to tickets for the most anticipated events. Whether you're a fan of BTS, BLACKPINK, Stray Kids, or other top groups, we've got you covered.<br/><br/>  GENERAL REGULATIONS OF THE EVENT<br/></span>
+    </div>
+)
+
+}
+    export default Detail;
